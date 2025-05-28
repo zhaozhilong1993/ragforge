3d12bf113f3ed296eec4391a1f213e6199469373
root <root@localhost.localdomain>
2025-05-29 00:53:48 +0800

    更新帮助启动的脚本

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -1,3 +1,6 @@
+#/etc/sysctl.conf最后一行追加
+#vm.max_map_count=2097152
+#sysctl -w vm.max_map_count=2097152
 cd ../
 export HF_ENDPOINT=https://hf-mirror.com
 export https_proxy=http://81.70.135.187:8118
