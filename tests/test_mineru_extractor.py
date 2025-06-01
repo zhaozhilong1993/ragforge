f309a06063ea9d83d6b1ea5572ce3cca40133ef8
root <cumtb_maxiao@163.com>
2025-06-01 22:23:03 +0800

    增加对VLM的测试文件

---
+++ tests/test_mineru_extractor.py
@@ -0,0 +1,207 @@
+import json
+import uuid
+import sys
+import os
+#os.environ['CUDA_VISIBLE_DEVICES']="6,7"
+#os.environ['SSL_CERT_FILE']='/etc/nginx/public.crt'
+#sys.path.append("/usr/local/lib/python3.10/dist-packages/")
+import time
+from datetime import datetime
+import requests
+import re
+import numpy as np
+from loguru import logger
+from magic_pdf.data.data_reader_writer import FileBasedDataWriter, FileBasedDataReader
+from magic_pdf.data.dataset import PymuDocDataset
+from magic_pdf.model.doc_analyze_by_custom_model import doc_analyze
+from magic_pdf.config.enums import SupportedPdfParseMethod
+import pandas as pd
+from magic_pdf.data.data_reader_writer import *
+from magic_pdf.data.data_reader_writer import MultiBucketS3DataReader
+from magic_pdf.data.schemas import S3Config
+from magic_pdf.data.data_reader_writer import S3DataReader, S3DataWriter
+from magic_pdf.data.dataset import PymuDocDataset
+from magic_pdf.model.doc_analyze_by_custom_model import doc_analyze
+import os
+from openai import OpenAI
+import base64
+
+from timeit import default_timer as timer
+
+logger.add("../for-debug.log", level="DEBUG")
+
+import boto3
+print('boto3',boto3.__file__)
+import fitz
+from PIL import Image
+from pypdf import PdfReader as pdf2_read
+from io import BytesIO
+
+start_ts = timer()
+bucket_name = "maxiao"
+ak = "D6Mdnsb3HvpyEVLQmmOX"
+sk = "kUkrVtKBCwdRycKbobHygRI7QBdw0no38gW8Gqef"
+endpoint_url = "https://101.52.216.178:19000/"
+reader = S3DataReader('/tmp/images/', bucket_name, ak, sk, endpoint_url)
+writer = S3DataWriter('tmp/', bucket_name, ak, sk, endpoint_url)
+image_writer = S3DataWriter('tmp/images', bucket_name, ak, sk, endpoint_url)
+#读取文件
+pdf_file_name='FPGA抗辐射加固技术.pdf'
+pdf_bytes = reader.read(pdf_file_name)
+#print(pdf_bytes)
+pdf_bytes_new = pdf_bytes
+
+pdf_doc = fitz.open('pdf', pdf_bytes_new)
+img_results = []
+
+import uuid
+import io
+
+
+
+def get_uuid():
+    return uuid.uuid1().hex
+
+#  base 64 编码格式
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+tmp_dir = ("./tmp")
+if not os.path.exists(tmp_dir):
+    os.mkdir(tmp_dir)
+path = None
+for page_num in range(len(pdf_doc)):
+    if page_num > 10:
+        break
+    page = pdf_doc.load_page(page_num)
+    # 将PDF页面转换为高质量图像（调整dpi参数根据需要）
+    mat = fitz.Matrix(2.0, 2.0)  # 缩放因子，提高分辨率
+    pix = page.get_pixmap(matrix=mat)
+
+    # 转换为PIL Image对象
+    img_bytes = pix.tobytes()
+    img = Image.open(BytesIO(img_bytes))
+    path = os.path.join(tmp_dir, "%s.jpg" % get_uuid())
+    img.save(path)
+    base64_image = encode_image(path)
+    img_results.append(base64_image)
+
+
+result = [] 
+for base64_image in img_results:
+    
+    client = OpenAI(
+        #api_key=os.getenv("DASHSCOPE_API_KEY"),
+        api_key = "sk-d065862c06754b26813e9d220b9a4f18",#sk-886e90542f6e4f90b13d295ebbcfe739",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+    
+    fields = ['标题','作者','发布时间','资料来源','摘要'] 
+    fields = ['目录'] 
+    completion = client.chat.completions.create(
+        model="qwen-vl-max-latest", # 此处以qwen-vl-max-latest为例，可按需更换模型名称。模型列表：https://help.aliyun.com/model-studio/getting-started/models
+        messages=[
+            {
+                "role": "system",
+                "content": [], #{"type": "text", "text": f"提取图中的：{fields}，请你以JSON格式输出，不要输出```json```代码段。"}],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url":{
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                    },
+                    {"type": "text", "text": f"现在输入的图片，有可能是文档的目录索引，也有可能是正文章节，也有可能都不是，请先判断是不是文档的纯粹的目录索引，如果是，请提取图中的：{fields}，以输出目录中的各个章节所对应的页码;如果不是纯粹的目录索引，不要提取。请你以JSON格式输出，不要输出```json```代码段。"},
+                ],
+            },
+        ],
+    )
+    result_t = completion.choices[0].message.content 
+    if result_t:
+        result.append(result_t)
+    #print(completion.choices[0].message.content)
+current_page_index = 0
+page_end = 0
+dic_result  = []
+index_id = 0
+page_numbers = []
+for r in result:
+    r_d = json.loads(r)
+    index_id = index_id +1
+    if '目录' in r_d:
+        print(r_d['目录'])
+        for i in r_d['目录']:
+            obj = {}
+            obj['章节'] = i['章节']
+            obj['页码'] = i['页码']
+            if obj['页码'] and obj['页码']>= current_page_index:
+                dic_result.append(obj)
+                current_page_index = obj['页码']
+                page_end = index_id
+            else:
+                continue
+print('章节页码结果{}, 从index_id {}开始是正文'.format(dic_result,page_end))
+
+
+for i in dic_result:
+    page_numbers.append(i['页码']+(page_end-1))
+    print('结果页面 {}'.format(page_numbers))
+
+
+img_results = []
+
+pdf_doc = fitz.open('pdf', pdf_bytes_new)
+for page_num in page_numbers:
+    page = pdf_doc.load_page(page_num)
+    # 将PDF页面转换为高质量图像（调整dpi参数根据需要）
+    mat = fitz.Matrix(2.0, 2.0)  # 缩放因子，提高分辨率
+    pix = page.get_pixmap(matrix=mat)
+
+    # 转换为PIL Image对象
+    img_bytes = pix.tobytes()
+    img = Image.open(BytesIO(img_bytes))
+    path = os.path.join(tmp_dir, "%s.jpg" % get_uuid())
+    img.save(path)
+    base64_image = encode_image(path)
+    img_results.append(base64_image)
+
+for base64_image in img_results:
+    
+    client = OpenAI(
+        #api_key=os.getenv("DASHSCOPE_API_KEY"),
+        api_key = "sk-d065862c06754b26813e9d220b9a4f18",#sk-886e90542f6e4f90b13d295ebbcfe739",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+    
+    fields = ['标题','作者','作者机构','摘要','关键词'] 
+    completion = client.chat.completions.create(
+        model="qwen-vl-max-latest", # 此处以qwen-vl-max-latest为例，可按需更换模型名称。模型列表：https://help.aliyun.com/model-studio/getting-started/models
+        messages=[
+            {
+                "role": "system",
+                "content": [{"type": "text", "text": f"提取图中的：{fields}，请你以JSON格式输出，不要输出```json```代码段。"}],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url":{
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                    },
+                    {"type": "text", "text": f"提取图中的：{fields}，请你以JSON格式输出，不要输出```json```代码段。"},
+                ],
+            },
+        ],
+    )
+    result_t = completion.choices[0].message.content 
+    print(result_t)
+
+task_time_cost = timer() - start_ts 
+print("花费时间 {}".format(task_time_cost))
--- tests/test_vlm/test_openai_vlm.py
+++ tests/test_vlm/test_openai_vlm.py
@@ -1,9 +1,17 @@
 import os
 from openai import OpenAI
 
+import base64
+
+#  base 64 编码格式
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+base64_image = encode_image("/var/lib/gpustack/ragflow/ragflow/tests/tmp/c5c310783e9011f0a89298039b6e4778.jpg")
+
 client = OpenAI(
     #api_key=os.getenv("DASHSCOPE_API_KEY"),
-    api_key = "sk-886e90542f6e4f90b13d295ebbcfe739",
+    api_key = "sk-d065862c06754b26813e9d220b9a4f18",#sk-886e90542f6e4f90b13d295ebbcfe739",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
@@ -20,7 +28,8 @@ completion = client.chat.completions.create(
                 {
                     "type": "image_url",
                     "image_url": {
-                        "url": "http://101.52.216.178:19000/8484f36c2cbe11f09962f6470b0b23dc/minerU/WechatIMG3140.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=U22S01P37H16EQFJCNF5%2F20250515%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250515T130730Z&X-Amz-Expires=43200&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJVMjJTMDFQMzdIMTZFUUZKQ05GNSIsImV4cCI6MTc0NzM1NjczMiwicGFyZW50IjoicmFnX2Zsb3cifQ.XHPb1m4Wf19aArVYzRW2fcJ9JNT7XzZuLlZeGkBL0fbbQy8mKS7BeCOiLJyabA0r-6oG6vExMUil5HxNJ3hnKw&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=0014d76785d9671d84d1f24ff6aa3e5698f58dee1947853e7df9bf2b939c58b4"
+                        "url": f"data:image/jpeg;base64,{base64_image}"
+                        #"url": "http://101.52.216.178:19000/8484f36c2cbe11f09962f6470b0b23dc/minerU/WechatIMG3140.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=U22S01P37H16EQFJCNF5%2F20250515%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250515T130730Z&X-Amz-Expires=43200&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJVMjJTMDFQMzdIMTZFUUZKQ05GNSIsImV4cCI6MTc0NzM1NjczMiwicGFyZW50IjoicmFnX2Zsb3cifQ.XHPb1m4Wf19aArVYzRW2fcJ9JNT7XzZuLlZeGkBL0fbbQy8mKS7BeCOiLJyabA0r-6oG6vExMUil5HxNJ3hnKw&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=0014d76785d9671d84d1f24ff6aa3e5698f58dee1947853e7df9bf2b939c58b4"
                     },
                 },
                 {"type": "text", "text": "提取图中的：['标题','作者','发布时间','资料来源','摘要']，请你以JSON格式输出，不要输出```json```代码段。"},
