e30778e072cb6d06d954bee2f0789f9ae738634b
root <cumtb_maxiao@163.com>
2025-06-03 20:05:25 +0800

    更新镜像拷贝脚本

---
+++ tools/export_docker_image_and_save.sh
@@ -0,0 +1,16 @@
+docker save -o ragflow_deps.tar infiniflow/ragflow_deps:latest
+docker save -o ragflow.tar infiniflow/ragflow:nightly
+docker save -o mysql.tar mysql:8.0.39
+docker save  -o  minio.tar quay.io/minio/minio:RELEASE.2023-12-20T01-00-02Z
+docker save  -o  elasticsearch.tar  elasticsearch:8.11.3
+docker save  -o  valkey.tar   valkey/valkey:8
+
+
+##
+#docker load -i ./ragflow_deps.tar 
+#docker load -i ./ragflow.tar
+#docker load -i ./mysql.tar
+#docker load -i ./minio.tar
+#docker load -i ./elasticsearch.tar
+#docker load -i ./valkey.tar
+#
