b37eaedfb21b08d4df86e420ee69753916f1607d
root <root@localhost.localdomain>
2025-06-05 12:55:34 +0800

    更新依赖的版本

--- tools/export_docker_image_and_save.sh
+++ tools/export_docker_image_and_save.sh
@@ -3,8 +3,8 @@ docker save -o ragflow.tar infiniflow/ragflow:nightly
 docker save -o mysql.tar mysql:8.0.39
 docker save  -o  minio.tar quay.io/minio/minio:RELEASE.2023-12-20T01-00-02Z
 docker save  -o  elasticsearch.tar  elasticsearch:8.11.3
-docker save  -o  valkey.tar   valkey/valkey:8
-docker save -o kes.tar minio/kes:latest
+docker save -o kes.tar minio/kes:0.22.0
+docker save  -o  valkey.tar   valkey/valkey:1.15.5
 
 ##
 #docker load -i ./ragflow_deps.tar 
