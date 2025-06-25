77dd4483d890aed3937202e2edbc381b1b0bce1f
root <root@localhost.localdomain>
2025-06-25 12:46:00 +0800

    更新视觉大模型测试脚本

--- tests/curl_test_models.sh
+++ tests/curl_test_models.sh
@@ -7,13 +7,12 @@ curl http://118.193.126.254:8080/embed -X POST -d '@./embedding.json' -H 'Conten
 echo "\n"
 
 echo '现在测试VL 32B视觉大模型'
-curl http://118.193.126.254:3525/v1/chat/completions -d '@./qwen_vlm.json'
+curl -X POST -H 'Content-Type: application/json' http://118.193.126.254:3525/v1/chat/completions -d '@./qwen_vlm.json'
 echo "\n"
 
 echo "现在测试Rerank模型"
 curl 118.193.126.254:1111/rerank -X POST  -d "@./rerank.json" -H 'Content-Type: application/json'
 echo "\n"
-
 
 echo "现在测试DeepSeek 70B大模型"
 curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST -d '@./deepseek_70b_llm.json' http://118.193.126.254:1025/v1/chat/completions
@@ -21,5 +20,5 @@ echo "\n"
 
 #本环境不使用 32B Deepseek
 #echo "现在测试DeepSeek 32B大模型"
-curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST -d '@./deepseek_32b_llm.json' http://118.193.126.254:1025/v1/chat/completions
-echo "\n"
+#curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST -d '@./deepseek_32b_llm.json' http://118.193.126.254:1025/v1/chat/completions
+#echo "\n"
