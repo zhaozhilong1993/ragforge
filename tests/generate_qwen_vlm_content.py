ad5cd1d1b45221809cb04870011aa41f5e2bfbd6
root <root@localhost.localdomain>
2025-06-25 12:44:54 +0800

    更新视觉大模型测试代码

--- tests/generate_qwen_vlm_content.py
+++ tests/generate_qwen_vlm_content.py
@@ -11,6 +11,13 @@ import random
 import textwrap
 import sys
 
+import base64
+
+#  base 64 编码格式
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+base64_image = encode_image("./FPGA抗辐射加固技术_02.png")
 def create_json():
     # 构建 JSON 结构
     json_data = {
@@ -18,18 +25,18 @@ def create_json():
         "messages": [{
         "role": "user",
         "content": [
-        {"type": "image_url", "image_url": "/model/hua.jpg"},
-        {"type": "text", "text": "请描述下图片内容"}
+        {"type": "image_url", "image_url": {"url":f"data:image/jpeg;base64,{base64_image}"}},
+        {"type": "text", "text": "提取图中的：['标题','作者','发布时间','资料来源','摘要']，请你以JSON格式输出，不要输出```json```代码段。"}
         ]
         }],
-        "max_tokens": 512,
+        #"max_tokens": 512,
         "do_sample": True,
         "repetition_penalty": 1.00,
         "temperature": 0.01,
         "top_p": 0.001,
         "top_k": 1
     }
-
+    print(json_data)
     # 输出到 JSON 文件（UTF-8 编码）
     filename = "qwen_vlm.json"
     with open(filename, "w", encoding="utf-8") as f:
